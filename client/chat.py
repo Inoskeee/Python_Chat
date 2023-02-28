7e190b620c10d73b1edc4984b492fa4a982300b9
Popov Nikita <58226087+Inoskeee@users.noreply.github.com>
2023-03-01 01:58:32 +0500

    completed tasks

--- client/chat.py
+++ client/chat.py
@@ -21,14 +21,14 @@ def set_timeout(delay, callback):
     asyncio.get_running_loop().call_later(delay, sync)
 
 mes_ids = []
-
-last_seen_id = 0
+user_ids = []
 
 send_message = js.document.getElementById("send_message")
 connect_chat = js.document.getElementById("connect_chat")
 sender = js.document.getElementById("sender")
 message_text = js.document.getElementById("message_text")
 chat_window = js.document.getElementById("chat_window")
+chat_users = js.document.getElementById("chat_users")
 
 # Добавляет новое сообщение в список сообщений
 def append_message(message):
@@ -37,10 +37,51 @@ def append_message(message):
         # Создаем HTML-элемент представляющий сообщение
         item = js.document.createElement("li")  # li - это HTML-тег для элемента списка
         item.className = "list-group-item"   # className - определяет как элемент выглядит
+        item.id = f'message_{message["msg_id"]}'
         # Добавляем его в список сообщений (chat_window)
-        item.innerHTML = f'[<b>{message["sender"]}</b>]: <span>{message["text"]}</span><span class="badge text-bg-light text-secondary">{message["time"]}</span>'
+        if message['sender'] == sender.value:
+
+            removeButton = js.document.createElement("a")
+            removeButton.className = "btn btn-primary"
+            removeButton.id = message["msg_id"]
+            removeButton.innerHTML = "Удалить у себя"
+            removeButton.onclick = remove_self_message
+
+            removeButtonAll = js.document.createElement("a")
+            removeButtonAll.className = "btn btn-warning"
+            removeButtonAll.id = message["msg_id"]
+            removeButtonAll.innerHTML = "Удалить у всех"
+            removeButtonAll.onclick = remove_message_all
+
+            item.innerHTML = f'[<b>{message["sender"]}</b>]: <span>{message["text"]}</span>' \
+                             f'<span class="badge text-bg-light text-secondary">{message["time"]}</span>'
+            item.append(removeButton)
+            item.append(removeButtonAll)
+        else:
+            item.innerHTML = f'[<b>{message["sender"]}</b>]: <span>{message["text"]}</span><span class="badge text-bg-light text-secondary">{message["time"]}</span>'
         chat_window.prepend(item)
         mes_ids.append(message["msg_id"])
+
+def append_users(user):
+    if user['id'] not in user_ids:
+        item = js.document.createElement("span")  # li - это HTML-тег для элемента списка
+        item.className = "input-group-text"  # className - определяет как элемент выглядит
+        # Добавляем его в список сообщений (chat_window)
+        item.innerHTML = f'{user["sender"]}'
+        chat_users.append(item)
+        user_ids.append(user['id'])
+
+
+async def remove_self_message(e):
+    id = e.target.id
+    message = js.document.getElementById(f'message_{id}')
+    message.remove()
+
+async def remove_message_all(e):
+    id = e.target.id
+    message = js.document.getElementById(f'message_{id}')
+    message.remove()
+    await fetch(f"/remove_message?id={id}", method="GET")
 
 # Вызывается при клике на send_message
 async def send_message_click(e):
@@ -55,19 +96,37 @@ async def connect_chat_click(e):
     message_text.hidden = False
     send_message.hidden = False
     chat_window.hidden = False
+    chat_users.hidden = False
+    await fetch(f"/connect_user?sender={sender.value}", method="GET")
     await load_fresh_messages()
 
 # Загружает новые сообщения с сервера и отображает их
 async def load_fresh_messages():
-    global last_seen_id
+    global sender
     # 1. Загружать все сообщения каждую секунду (большой трафик)
-    result = await fetch(f"/get_messages?after={last_seen_id}", method="GET")  # Делаем запрос
+    result = await fetch(f"/get_messages?sender={sender.value}", method="GET")  # Делаем запрос
+    users = await fetch(f"/get_users", method="GET")  # Делаем запрос
+
+    usersData = await users.json()
+    connected_users = usersData["users"]
+    for user in connected_users:
+        append_users(user)
     # chat_window.innerHTML = ""  # Очищаем окно с сообщениями
     data = await result.json()
     all_messages = data["messages"]  # Берем список сообщений из ответа сервера
+
+    last_seen_ids = []
     for msg in all_messages:
-        last_seen_id = msg["msg_id"]  # msg_id Последнего сообщение
+        last_seen_ids.append(msg["msg_id"])  # msg_id Последнего сообщение
         append_message(msg)
+    try:
+        for msg in mes_ids:
+            if msg not in last_seen_ids:
+                message = js.document.getElementById(f'message_{msg}')
+                if message != None:
+                    message.remove()
+    except Exception as e:
+        print(e)
     set_timeout(1, load_fresh_messages) # Запускаем загрузку заново через секунду
     # 2. Загружать только новые сообщения
 
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ app = Flask(__name__, static_folder="./client", template_folder="./client")  # �
 msg_id = 1
 all_messages = []
 
+usr_id = 1
+users = []
 
 @app.route("/chat")
 def chat_page():
@@ -17,20 +19,55 @@ def add_message(sender, text):
     new_message = {
         "sender": sender,
         "text": text,
-        "time": datetime.now().strftime("%H:%M"),
+        "time": datetime.now(),
         "msg_id": msg_id
     }
     msg_id += 1
     all_messages.append(new_message)
-
+    message = f"{new_message['time']} - {new_message['sender']}: {new_message['text']}\n"
+    with open('allmessages.txt', 'a') as file:
+        file.write(message)
 
 # API для получения списка сообщений
 
 @app.route("/get_messages")
 def get_messages():
+    sender = request.args["sender"]
+    connect_time = None
+    for user in users:
+        if user["sender"] == sender:
+            connect_time = user["connect_time"]
+    messages = []
+    for message in all_messages:
+        if message["time"] >= connect_time:
+            messages.append(message)
+    return {"messages": messages}
 
-    return {"messages": all_messages}
+@app.route("/connect_user")
+def connect_user():
+    global usr_id
+    sender = request.args["sender"]
+    users.append({"id": usr_id, "sender" : sender, "connect_time": datetime.now()})
+    usr_id += 1
+    return users
 
+@app.route("/get_users")
+def get_users():
+    return {"users": users}
+
+@app.route("/remove_message")
+def remove_message():
+    id = request.args["id"]
+    index = None
+    for i in range(0, len(all_messages)):
+        if all_messages[i]['msg_id'] == int(id):
+            index = i
+            break
+    print(all_messages[0])
+    print(index)
+    if index != None:
+        all_messages.pop(index)
+    return id
 
 # HTTP-GET
 # API для получения отправки сообщения  /send_message?sender=Mike&text=Hello
